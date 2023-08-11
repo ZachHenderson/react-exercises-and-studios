6418943ae455aaa49bfe1879d599d474d7ce9f19
Zach <amidstbooks@gmail.com>
2023-08-10 20:17:40 -0500

    All the steps

--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,7 +1,7 @@
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://joyfoodsunshine.com/homemade-mac-and-cheese/";
+   let authorPhoto = "https://joyfoodsunshine.com/wp-content/uploads/2023/04/cropped-author-avatar-64x64.jpg";
+   let authorName = "Laura";
 
    return (
       <div>
@@ -15,7 +15,7 @@ const RecipeAuthor = () => {
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["macaroni", "butter", "milk", "cheese", "corn starch"];
    return(
       <div>
          <h3>Recipe Ingredients</h3>
@@ -34,8 +34,9 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Easy Homemade Mac and Cheese</h1>
+            <p>This Easy Homemade Mac and Cheese Recipe is made with 8 ingredients in 15 minutes on the stovetop!</p>
+            <img src="https://joyfoodsunshine.com/wp-content/uploads/2019/04/homemade-mac-and-cheese-recipe-1.jpg" alt="Bowl of mac and cheese"/>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
