f102bf612eb0087686285f22a78b98c5ef15f65b
Zach <amidstbooks@gmail.com>
2023-08-10 20:52:36 -0500

    All the steps

---
+++ part3/exercises/src/components/Introduction.js
@@ -0,0 +1,12 @@
+export default function HobbyIntroduction() {
+    return(
+        <div>
+            <h2>3 Fun Facts About this Hobby</h2>
+            <ul>
+                <li>It creates denser bones</li>
+                <li>It lowers morbidity</li>
+                <li>It raises metabolism</li>
+            </ul>
+        </div>
+    )
+}
